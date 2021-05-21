b3c4829a6e2072b249a049e09611577c6546ec05
影山榮一 <a1kageyama@kageyamBookpuro.AirPort>
2021-05-21 10:02:55 +0900

    Update

--- LinqList.cs
+++ LinqList.cs
@@ -2,124 +2,100 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-//This Code is need a mono.
-
 namespace LinqList
 {
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-			var List = new ListClass();
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var listClass = new ListClass();
 
-			List.FindLength();
+            listClass.FindLength();
 
-			List.ExistsS();
+            listClass.ExistsS();
 
-			List.FindIndexIwate();
+            listClass.FindIndexVermont();
 
-			List.FindAllFive();
+            listClass.FindAllFive();
 
-			List.RemoveHi();
+            listClass.RemoveAl();
 
-			// RemoveHi関数で除外しているため、リストの数は変わっている
-			List.FindIndexIwate();
+            // The number of lists has changed because of exclusion by the RemoveAl function
+            listClass.FindIndexVermont();
 
-			List.ForeachConsole();
+            listClass.ForeachConsole();
 
-			// 出力が小文字になるだけで、リストそのものは小文字にならない
-			List.ConvertAllString();
+            // Only the output will be lowercased, the list itself will not be lowercased
+            listClass.ConvertAllString();
 
-			Console.WriteLine();
+            Console.WriteLine();
 
-			List.ForeachConsole();
-			
-		}
-	}
+            listClass.ForeachConsole();
+        }
+    }
 
-	class ListClass
-	{
-		List<string> list = new List<string>
-		{
-			"Tokyo", "Saitama", "Tochigi", "Ibaraki", "Gunma", "Chiba", "Kanagawa", 
-			"Shizuoka", "Shiga", "Nagano", "Yamaguchi", "Kouchi", "Aomori", "Iwate", 
-		};         
+    class ListClass
+    {
+        List<string> list = new List<string>
+        {
+            "Alaska", "Alabama", "Colorado", "Delaware", "Georgia", "Iowa", "Kentucky", 
+            "Massachusetts", "Maryland", "Minnesota", "Nevada", "Oklahoma", "Pennsylvania", "Tennessee", 
+            "Texas", "Utah", "Vermont", "Washington", "Wisconsin", "WestVirginia", "Wyoming", 
+        };
 
-		// 文字数が6の県名を抽出
-		public void FindLength()
-		{
-			var name = list.Find(s => s.Length == 6);
-			Console.WriteLine(name);
-			Console.WriteLine();
-		}
+        // Extract the states with 7 characters
+        public void FindLength()
+        {
+            var name = list.Find(s => s.Length == 7);
+            Console.WriteLine(name);
+            Console.WriteLine();
+        }
 
-		// 頭文字がSの県名が、あるorない
-		public void ExistsS()
-		{
-			var exists = list.Exists(s => s[0] == 'S');
-			Console.WriteLine(exists);
-			Console.WriteLine();
-		}
+        // There are states with the initial letter S or not
+        public void ExistsS()
+        {
+            var exists = list.Exists(s => s[0] == 'S');
+            Console.WriteLine(exists);
+            Console.WriteLine();
+        }
 
-		// 岩手県の格納インデックス数を取得
-		public void FindIndexIwate()
-		{
-			int index = list.FindIndex(s => s == "Iwate");
-			Console.WriteLine(index);
-			Console.WriteLine();
-		}
+        // Get the number of stored indexes for Vermont
+        public void FindIndexVermont()
+        {
+            int index = list.FindIndex(s => s == "Vermont");
+            Console.WriteLine(index);
+            Console.WriteLine();
+        }
 
-		// 文字数が5文字の県名を取得
-		public void FindAllFive()
-		{
-			var names = list.FindAll(s => s.Length <= 5);
-			foreach (var s in names)
-				Console.WriteLine(s);
-			Console.WriteLine();
-		}
+        // Get the states with 4 characters
+        public void FindAllFive()
+        {
+            var names = list.FindAll(s => s.Length <= 4);
+            foreach (var s in names)
+                Console.WriteLine(s);
+            Console.WriteLine();
+        }
 
-		// hiの文字列が入っている県名を除外し、削除した数を取得
-		public void RemoveHi()
-		{
-			var removeCount = list.RemoveAll(s => s.Contains("hi"));
-			Console.WriteLine(removeCount);
-			Console.WriteLine();
-		}
+        // Exclude states that contain the string Al and get the number removed
+        public void RemoveAl()
+        {
+            var removeCount = list.RemoveAll(s => s.Contains("Al"));
+            Console.WriteLine(removeCount);
+            Console.WriteLine();
+        }
 
-		// リストの各要素を出力
-		public void ForeachConsole()
-		{
-			list.ForEach(s => Console.WriteLine(s));
-			Console.WriteLine();
-		}
+        // Output each element of the list
+        public void ForeachConsole()
+        {
+            list.ForEach(s => Console.WriteLine(s));
+            Console.WriteLine();
+        }
 
-		// 小文字に変換
-		public void ConvertAllString()
-		{
-			var lowerList = list.ConvertAll(s => s.ToLower());
-			lowerList.ForEach(s => Console.WriteLine(s));
-		}
-
-	}
+        // Convert to lower case
+        public void ConvertAllString()
+        {
+            var lowerList = list.ConvertAll(s => s.ToLower());
+            lowerList.ForEach(s => Console.WriteLine(s));
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
